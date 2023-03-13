b29e489d1d2ddd6badaf17ffc9044819392d8a68
Evhenii Khartaniuk <co6alt@gmail.com>
2023-03-13 12:17:01 +0200

    hw 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+// var throttle = require('lodash.throttle');
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// function timeUpdateFunc(time) {
+//   console.log(time);
+//   localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+// }
+
+// player.on('timeupdate', throttle(timeUpdateFunc, 1200));
+
+// const saveTime = localStorage.getItem('videoplayer-current-time');
+// const timeStop = JSON.parse(saveTime);
+// player.setCurrentTime(timeStop.seconds || 0);
+
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(saveTime, 1000));
+
+function saveTime(event) {
+  let seconds = event.seconds;
+
+  localStorage.setItem(CURRENT_TIME_KEY, seconds);
+}
+
+const currentVideoTime = localStorage.getItem(CURRENT_TIME_KEY);
+
+if (currentVideoTime) {
+  player.setCurrentTime(currentVideoTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+var throttle = require('lodash.throttle');
 
+const formEl = document.querySelector('.feedback-form ');
+const emailEl = document.querySelector('.feedback-form input');
+const messageEl = document.querySelector('.feedback-form textarea');
+
+formEl.addEventListener('input', throttle(formFeedBack, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+const userSavedLocalStorage = JSON.parse(
+  localStorage.getItem('feedback-form-state')
+);
+
+if (userSavedLocalStorage) {
+  messageEl.value = userSavedLocalStorage.message;
+  emailEl.value = userSavedLocalStorage.email;
+}
+
+function formFeedBack() {
+  let formEmailValue = emailEl.value;
+  let formMessageValue = messageEl.value;
+
+  const userLocalStorageData = {
+    email: formEmailValue,
+    message: formMessageValue,
+  };
+
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(userLocalStorageData)
+  );
+}
+
+function onFormSubmit(events) {
+  events.preventDefault();
+  formEl.reset();
+
+  console.log(localStorage);
+
+  localStorage.removeItem('feedback-form-state');
+}
